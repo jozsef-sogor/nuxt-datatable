ec16de53d0a4cd42f38473ca4f4f968ed5176935
jozsef-sogor <44443711+jozsef-sogor@users.noreply.github.com>
2022-08-10 19:56:17 +0200

    Server side pagination added

--- components/DataTable.vue
+++ components/DataTable.vue
@@ -1,18 +1,42 @@
 <template lang="pug">
   v-data-table(
-    :headers="headers"
-    :items="items"
     item-key="email"
     dense
+    :headers="headers"
+    :items="items"
+    :options.sync="sortOptions"
+    :server-items-length="totalItems"
+    :loading="isLoading"
+    @update:page="updateTable"
+    @update:sortBy="updateTable"
+    @update:options="updateTable"
   ).elevation-1.mt-10
+    template(v-slot:top)
+      v-text-field(
+        v-model="search"
+        label="Search data"
+        class="mx-4"
+      )
 </template>
 
 <script>
 export default {
-  props: ['headers', 'items'],
+  props: ['headers', 'items', 'totalItems', 'isLoading', 'currentPage'],
   data() {
     return {
-      
+      search: '',
+      sortOptions: {}
+    }
+  },
+  methods: {
+    updateTable() {
+      const updatedOptions = {
+        page: this.sortOptions.page,
+        itemsPerPage: this.sortOptions.itemsPerPage,
+        sortBy: this.sortOptions.sortBy.length ? this.sortOptions.sortBy[0] : null,
+        sortDesc: this.sortOptions.sortDesc.length ? this.sortOptions.sortDesc[0] : false
+      }
+      this.$emit('updateTable', updatedOptions)
     }
   }
 }
--- pages/index.vue
+++ pages/index.vue
@@ -6,6 +6,10 @@
           v-if="items.length"
           :headers="headers"
           :items="items"
+          :totalItems="totalItemsLength"
+          :currentPage="currentPage"
+          :isLoading="isLoading"
+          @updateTable="fetchData"
         )
         v-progress-circular(
           v-else
@@ -27,6 +31,9 @@ export default {
     return {
       sales,
       items: [],
+      totalItemsLength: 0,
+      currentPage: 1,
+      isLoading: false,
       headers: [
         { text: 'Name', value: 'user', align: 'start' },
         { text: 'Email', value: 'email' },
@@ -34,17 +41,54 @@ export default {
         { text: 'Year', value: 'year' },
         { text: 'Sales', value: 'sales' },
         { text: 'Country', value: 'country' },
-      ],
+      ]
     }
   },
   async created() {
-    this.items = await this.fetchData(0, 50)
+    await this.fetchData({})
+    //this.items = response.data
+    //this.totalItemsLength = response.total
+    //this.currentPage = response.currentPage
   },
   methods: {
-    async fetchData(page, size) {
-      const start = page * size
+    async fetchData({page = 1, itemsPerPage = 10, sortBy = null, sortDesc = false, query = null}) {
+      //Set loading state to DataTable
+      this.isLoading = true
+      //Get queryString from options
+      const queryString = new URLSearchParams({
+        page: page,
+        itemsPerPage: itemsPerPage,
+        sortBy: sortBy,
+        sortDesc: sortDesc,
+        query: query
+      }).toString()
+      //Mock API call
+      const response = await this.mockServerCode(`?${queryString}`)
+      this.items = response.data
+      this.totalItemsLength = response.total
+      this.currentPage = response.currentPage
+
+      this.isLoading = false
+    },
+    async mockServerCode(queryString) {
+      //Get params object from query string
+      const urlParams = new URLSearchParams(queryString)
+      const reqParams = Object.fromEntries(urlParams)
+      //Set delay
       await this.delay(3000)
-      return await sales.results.slice(start, start + size)
+      //Create response
+      const start = (reqParams.page - 1) * reqParams.itemsPerPage //starting item in array
+      const resultsItems = await sales.results.slice(start, start + parseInt(reqParams.itemsPerPage))
+      const totalResultsLength = sales.results.length
+      //const filteredResults = []
+      const currentPage = reqParams.page
+      const resultsObj = {
+        total: totalResultsLength,
+        currentPage: parseInt(currentPage) ,
+        data: resultsItems
+      }
+
+      return resultsObj
     },
     delay(ms) {
       return new Promise(resolve => setTimeout(resolve, ms))
